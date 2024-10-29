0205e8fae1ca26ce78840ff30b7c8951013bedff
abhay2133 <abhaybishthestudent@gmail.com>
2024-10-29 20:43:28 +0530

    fixed eslint issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express with TypeScript!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, Express with TypeScript!");
 });
 
 app.listen(PORT, () => {
